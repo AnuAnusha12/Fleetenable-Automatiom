1075ec8dc645ebe280dfeadb1290594de6f356ff
AnuAnusha12 <anusha.b@imaginnovate.com>
2023-05-31 18:19:53 +0530

    initial commit

--- cypress/e2e/2-advanced-examples/cypress.config.js
+++ cypress/e2e/2-advanced-examples/cypress.config.js
@@ -6,10 +6,9 @@ module.exports = defineConfig({
 
   e2e: {
     specPattern: [
-      'cypress/e2e/2-advanced-examples/1Fe_Login.spec.js',
-      'cypress/e2e/2-advanced-examples/2Fe_Dashboard.spec.js',
+      'cypress/e2e/2-advanced-examples/1FeLogin.spec.js',
+      'cypress/e2e/2-advanced-examples/2FeDashboard.spec.js'
     ],
-    projectId: '4bajr8', // Set the projectId for recording
     recordKey: '323f9d3f-caaa-4904-a876-638017f85dd9', // Set your record key
   },
   // Rest of your configuration...
