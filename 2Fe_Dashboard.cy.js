985a0f11a6e925c066b22ea33eec13d8c8cd96d8
AnuAnusha12 <anusha.b@imaginnovate.com>
2023-05-31 17:15:28 +0530

    initial commit of the repositry

--- 2Fe_Dashboard.cy.js
+++ 2Fe_Dashboard.cy.js
@@ -1,11 +1,32 @@
-
-    it('should open dash board',() =>
-    {
-        cy.visit('https://app.fleetenable.com/dashboard')
-        cy.get('body').click()
-        cy.wait(4000);
-
-        cy.get(':nth-child(7) > .ant-menu-submenu-title').click()
-
-        
-    });
+ /// <reference types="cypress" />
+    describe('Dashboard Functionality', () => {
+        before(() => {
+          // Perform any setup or common actions needed for all test cases
+        });
+      
+        it('Should open the Dashboard screen', () => {
+          login();
+      
+          cy.visit('https://fe-beta.fleetenable.com/dashboard');
+          cy.wait(4000);
+          // Continue with assertions or interactions on the Dashboard screen
+          cy.get('.ant-select-selection__rendered').click();
+          cy.get('.ant-select-dropdown-menu > :nth-child(2)').click();
+          cy.get('.ant-menu-item > .active');
+          cy.get(':nth-child(3) > .ant-menu-submenu-title').click();
+          cy.wait(4000);
+          cy.get('.ant-btn.marginLeft10.ant-btn-primary').eq(0).click({ force: true });
+          cy.get('button[type="button"]').eq(0).wait(2000);
+          cy.get('.ant-calendar-picker-input');
+          cy.get('.MuiInputAdornment-root > .MuiButtonBase-root').type('08:30 AM', { force: true });
+          cy.get('[style="margin-bottom: 15px;"] > .ant-select > .ant-select-selection').then(() => {
+            cy.get('input[type="checkbox"]').first().check();
+            cy.get('.ant-table-thead > tr > .ant-table-selection-column').first();
+            cy.get('.marginRight5').click();
+          });
+      
+          after(() => {
+            // Perform any cleanup or common actions needed after all test cases
+          });
+        });
+      });
--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '4bajr8',
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: [
+      'cypress/e2e/2-advanced-examples/1Fe_Login.spec.js',
+      'cypress/e2e/2-advanced-examples/2Fe_Dashboard.spec.js',
+    ],
+    projectId: '4bajr8', // Set the projectId for recording
+    recordKey: '323f9d3f-caaa-4904-a876-638017f85dd9', // Set your record key
   },
+  // Rest of your configuration...
 });
